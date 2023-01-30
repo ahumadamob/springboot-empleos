1a913a9c0cc60e56bb7fa8c6aa9aa215874c4e0a
Mario Ahumada <ahumadamob@gmail.com>
2023-01-30 14:13:14 -0300

    Finalizado capítulo 11: Configurar la página de inicio de una aplicación Web

---
+++ src/main/java/com/ahumadamob/controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.ahumadamob.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+	
+	@GetMapping("/")
+	public String showHome() {
+		return "home";
+	}
+
+}
