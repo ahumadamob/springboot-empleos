29e628ff846348a55dd88b50d5135757cc00f344
Mario Ahumada <ahumadamob@gmail.com>
2023-01-31 19:37:35 -0300

    Capítulo 13: Agregar información al modelo para desplegarlo en la vista

--- src/main/java/com/ahumadamob/controller/HomeController.java
+++ src/main/java/com/ahumadamob/controller/HomeController.java
@@ -1,13 +1,18 @@
 package com.ahumadamob.controller;
 
+import java.util.Date;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 	
 	@GetMapping("/")
-	public String showHome() {
+	public String showHome(Model model) {
+		model.addAttribute("message", "Bienvenidos a Empleos App");
+		model.addAttribute("fecha", new Date());
 		return "home";
 	}
 
