0f2a36b322e8474f22a374f9707de6a78eb082e0
Mario Ahumada <ahumadamob@gmail.com>
2023-01-31 22:33:23 -0300

    Capítulo 18: Agregar un tipo de dato Vacante al modelo y desplegarlo en la vista

--- src/main/java/com/ahumadamob/controller/HomeController.java
+++ src/main/java/com/ahumadamob/controller/HomeController.java
@@ -8,8 +8,26 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.ahumadamob.model.Job;
+
 @Controller
 public class HomeController {
+	
+	@GetMapping("/details")
+	public String showDetails(Model model) {
+		
+		Job job = new Job();
+		
+		job.setTitle("Desarrollador SpringBoot Sr");
+		job.setDescription("Se solicita desarrollador SpringBoot Sr con manejo avanzado de inglés para trabajar en remoto");
+		job.setSalary(2800.0);
+		job.setPostDate(new Date());
+		job.setActive(true);
+		
+		model.addAttribute("job", job);		
+		
+		return "details";
+	}
 	
 	@GetMapping("/list")
 	public String showList(Model model) {
